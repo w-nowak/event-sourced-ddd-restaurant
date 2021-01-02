7038762737260c6ebcb8d26a6b393f83d90a5480
Wojciech Nowak <cartoonmail20@gmail.com>
2021-01-02 12:44:20 +0100

    Refactored AggregateRepositoryTest test class to use Fixture

--- shared-kernel/src/test/java/com/wnowakcraft/samples/restaurant/core/domain/model/AggregateRepositoryTest.java
+++ shared-kernel/src/test/java/com/wnowakcraft/samples/restaurant/core/domain/model/AggregateRepositoryTest.java
@@ -1,15 +1,15 @@
 package com.wnowakcraft.samples.restaurant.core.domain.model;
 
+import com.wnowakcraft.samples.restaurant.core.domain.model.Aggregate.Version;
 import com.wnowakcraft.samples.restaurant.core.domain.model.AggregateRepository.RestoreAggregateFromEvents;
 import com.wnowakcraft.samples.restaurant.core.domain.model.AggregateRepository.RestoreAggregateFromSnapshot;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Answers;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.concurrent.CompletableFuture;
 
 import static com.wnowakcraft.samples.restaurant.core.domain.model.ModelTestData.Aggregate;
@@ -22,67 +22,178 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 
 class AggregateRepositoryTest {
-    @Mock(answer = Answers.RETURNS_DEEP_STUBS)
-    private EventStore<Event, Aggregate, AggregateId> eventStore;
-    @Mock private SnapshotRepository<ModelTestData.Snapshot, ModelTestData.AggregateId> snapshotRepository;
-    @Mock private RestoreAggregateFromSnapshot<Event, Aggregate, Snapshot, AggregateId> restoreAggregateFromSnapshot;
-    @Mock private RestoreAggregateFromEvents<Event, Aggregate, AggregateId> restoreAggregateFromEvents;
-    private AggregateRepository<Event, Aggregate, Snapshot, AggregateId> aggregateRepository;
+    private Fixture fixture;
 
     @BeforeEach
     public void setUp() {
-        MockitoAnnotations.initMocks(this);
-        aggregateRepository =
-                new AggregateRepository<>(eventStore, snapshotRepository, restoreAggregateFromSnapshot, restoreAggregateFromEvents);
+        fixture = new Fixture();
+    }
+
+    @AfterEach
+    public void verifyInteractions() {
+        fixture.thenVerifyAllNecessaryInteractionsWithDependencies();
+        fixture.andVerifyNoMoreInteractionsWithTheseDependencies();
     }
 
     @Test
     void save_storesAggregateChangesIntoEventStore_andReturnsCorrectAggregateVersion() throws Exception {
-        given(eventStore.append(AGGREGATE.getId(), AGGREGATE.getVersion(), AGGREGATE.getChanges()))
-                .willReturn(CompletableFuture.completedFuture(AGGREGATE_VERSION_2));
+        fixture.givenAn(AGGREGATE);
+        fixture.givenAggregateChangesAreAppendedToEventStore();
 
-        var futureVersion = aggregateRepository.save(AGGREGATE);
+        fixture.whenSaveIsCalled();
 
-        assertThat(futureVersion.get()).isEqualTo(AGGREGATE_VERSION_2);
+        fixture.thenReturnedVersionIsNextAggregateVersion();
+        fixture.andVerifyNoInteractionWithDependenciesOtherThanEventStore();
     }
 
     @Test
     void getById_returnsAggregateRecreatedFromEvents_whenNoSnapshotIsAvailable() {
         var restoreEvents = List.<Event>of(AGGREGATE_INIT_EVENT, AGGREGATE_SAMPLE_EVENT);
-        given(snapshotRepository.findLatestSnapshotFor(AGGREGATE.getId())).willReturn(empty());
-        given(eventStore.loadAllEventsFor(AGGREGATE.getId())).willReturn(new EventStream(restoreEvents, AGGREGATE_VERSION_2));
-        given(restoreAggregateFromEvents.restore(restoreEvents, AGGREGATE_VERSION_2)).willReturn(AGGREGATE);
+        fixture.givenAn(AGGREGATE);
+        fixture.givenNoSnapshotIsAvailableForGivenAggregate();
+        fixture.givenEventStoreReturnsEventStreamWithAll(restoreEvents);
+        fixture.givenAggregateIsRestoredWithJustEvents();
 
-        var actualAggregate = aggregateRepository.getById(AGGREGATE.getId());
+        fixture.whenGetByIdIsCalled();
 
-        assertThat(actualAggregate).isEqualTo(AGGREGATE);
-        then(snapshotRepository).should().findLatestSnapshotFor(AGGREGATE.getId());
-        then(eventStore).should().loadAllEventsFor(AGGREGATE.getId());
-        then(restoreAggregateFromEvents).should().restore(restoreEvents, AGGREGATE_VERSION_2);
-
-        then(snapshotRepository).shouldHaveNoMoreInteractions();
-        then(eventStore).shouldHaveNoMoreInteractions();
-        then(restoreAggregateFromEvents).shouldHaveNoMoreInteractions();
-        then(restoreAggregateFromSnapshot).shouldHaveNoInteractions();
+        fixture.thenTheExpectedAggregateShouldBeReturned();
+        fixture.andVerifyNoInteractionWithRestoreAggregateFromSnapshotAndEvents();
     }
 
     @Test
     void getById_returnsAggregateRecreatedFromSnapshotAndEvents_whenSnapshotIsAvailable() {
-        var restoreEvents = List.<Event>of(AGGREGATE_INIT_EVENT, AGGREGATE_SAMPLE_EVENT);
-        given(snapshotRepository.findLatestSnapshotFor(AGGREGATE.getId())).willReturn(Optional.of(Snapshot.DEFAULT));
-        given(eventStore.loadEventsFor(AGGREGATE.getId(), AGGREGATE_VERSION_2)).willReturn(new EventStream(restoreEvents, AGGREGATE_VERSION_2));
-        given(restoreAggregateFromSnapshot.restore(Snapshot.DEFAULT, restoreEvents, AGGREGATE_VERSION_2)).willReturn(AGGREGATE);
+        var restoreEvents = List.<Event>of(AGGREGATE_SAMPLE_EVENT);
+        fixture.givenAn(AGGREGATE);
+        fixture.givenSnapshotIsAvailableForGivenAggregate();
+        fixture.givenEventStoreForGivenAggregateVersionReturnsEventStreamWith(restoreEvents);
+        fixture.givenAggregateIsRestoredWithSnapshotAndEvents();
 
-        var actualAggregate = aggregateRepository.getById(AGGREGATE.getId());
+        fixture.whenGetByIdIsCalled();
 
-        assertThat(actualAggregate).isEqualTo(AGGREGATE);
-        then(snapshotRepository).should().findLatestSnapshotFor(AGGREGATE.getId());
-        then(eventStore).should().loadEventsFor(AGGREGATE.getId(), AGGREGATE_VERSION_2);
-        then(restoreAggregateFromSnapshot).should().restore(Snapshot.DEFAULT, restoreEvents, AGGREGATE_VERSION_2);
+        fixture.thenTheExpectedAggregateShouldBeReturned();
+        fixture.andVerifyNoInteractionWithRestoreAggregateFromEvents();
+    }
 
-        then(snapshotRepository).shouldHaveNoMoreInteractions();
-        then(eventStore).shouldHaveNoMoreInteractions();
-        then(restoreAggregateFromSnapshot).shouldHaveNoMoreInteractions();
-        then(restoreAggregateFromEvents).shouldHaveNoInteractions();
+    @Test
+    void getById_returnsAggregateRecreatedFromSnapshotAndEmptyStreamOfEvents_whenSnapshotIsAvailable_andEmptyEventStreamIsReturned() {
+        var restoreEvents = Collections.<Event>emptyList();
+        fixture.givenAn(AGGREGATE);
+        fixture.givenSnapshotIsAvailableForGivenAggregate();
+        fixture.givenEventStoreForGivenAggregateVersionReturnsEventStreamWith(restoreEvents);
+        fixture.givenAggregateIsRestoredWithSnapshotAndEvents();
+
+        fixture.whenGetByIdIsCalled();
+
+        fixture.thenTheExpectedAggregateShouldBeReturned();
+        fixture.andVerifyNoInteractionWithRestoreAggregateFromEvents();
+    }
+
+    private static class Fixture {
+        @Mock private EventStore<Event, Aggregate, AggregateId> eventStore;
+        @Mock private SnapshotRepository<ModelTestData.Snapshot, ModelTestData.AggregateId> snapshotRepository;
+        @Mock private RestoreAggregateFromSnapshot<Event, Aggregate, Snapshot, AggregateId> restoreAggregateFromSnapshot;
+        @Mock private RestoreAggregateFromEvents<Event, Aggregate, AggregateId> restoreAggregateFromEvents;
+        private AggregateRepository<Event, Aggregate, Snapshot, AggregateId> aggregateRepository;
+
+        private Aggregate aggregate;
+        private Aggregate actualAggregate;
+        private Snapshot snapshot;
+        private Collection<Event> events;
+        private Version aggregateVersion;
+        private Collection<Runnable> interactions = new ArrayList<>();
+        private Collection<Object> interactionObjects = new ArrayList<>();
+
+        public Fixture() {
+            MockitoAnnotations.initMocks(this);
+            aggregateRepository =
+                    new AggregateRepository<>(eventStore, snapshotRepository, restoreAggregateFromSnapshot, restoreAggregateFromEvents);
+        }
+
+        public void givenAn(Aggregate aggregate) {
+            this.aggregate = aggregate;
+        }
+
+        public void givenAggregateChangesAreAppendedToEventStore() {
+            given(eventStore.append(aggregate.getId(), aggregate.getVersion(), aggregate.getChanges()))
+                    .willReturn(CompletableFuture.completedFuture(AGGREGATE_VERSION_2));
+            interactions.add(() -> then(eventStore).should().append(aggregate.getId(), aggregate.getVersion(), aggregate.getChanges()));
+        }
+
+        public void whenSaveIsCalled() throws Exception {
+            aggregateVersion = aggregateRepository.save(AGGREGATE).get();
+        }
+
+        public void thenReturnedVersionIsNextAggregateVersion() {
+            assertThat(aggregateVersion).isEqualTo(AGGREGATE_VERSION_2);
+        }
+
+        public void givenSnapshotIsAvailableForGivenAggregate() {
+            given(snapshotRepository.findLatestSnapshotFor(aggregate.getId())).willReturn(Optional.of(Snapshot.DEFAULT));
+            addInteraction(snapshotRepository, () -> then(snapshotRepository).should().findLatestSnapshotFor(aggregate.getId()));
+        }
+
+        public void givenNoSnapshotIsAvailableForGivenAggregate() {
+            given(snapshotRepository.findLatestSnapshotFor(aggregate.getId())).willReturn(empty());
+            addInteraction(snapshotRepository, () -> then(snapshotRepository).should().findLatestSnapshotFor(aggregate.getId()));
+        }
+
+        public void givenEventStoreReturnsEventStreamWithAll(List<Event> events) {
+            this.events = events;
+            given(eventStore.loadAllEventsFor(aggregate.getId())).willReturn(new EventStream(events, AGGREGATE_VERSION_2));
+            addInteraction(eventStore, () -> then(eventStore).should().loadAllEventsFor(aggregate.getId()));
+        }
+
+        public void givenEventStoreForGivenAggregateVersionReturnsEventStreamWith(List<Event> events) {
+            this.events = events;
+            given(eventStore.loadEventsFor(aggregate.getId(), AGGREGATE_VERSION_2)).willReturn(new EventStream(events, AGGREGATE_VERSION_2));
+            addInteraction(eventStore, () -> then(eventStore).should().loadEventsFor(aggregate.getId(), AGGREGATE_VERSION_2));
+        }
+
+        public void givenAggregateIsRestoredWithJustEvents() {
+            given(restoreAggregateFromEvents.restore(events, AGGREGATE_VERSION_2)).willReturn(aggregate);
+            addInteraction(restoreAggregateFromEvents, () -> then(restoreAggregateFromEvents).should().restore(events, AGGREGATE_VERSION_2));
+        }
+
+        public void givenAggregateIsRestoredWithSnapshotAndEvents() {
+            given(restoreAggregateFromSnapshot.restore(Snapshot.DEFAULT, events, AGGREGATE_VERSION_2)).willReturn(aggregate);
+            addInteraction(restoreAggregateFromSnapshot, () -> then(restoreAggregateFromSnapshot).should().restore(Snapshot.DEFAULT, events, AGGREGATE_VERSION_2));
+        }
+
+        public void whenGetByIdIsCalled() {
+            actualAggregate = aggregateRepository.getById(aggregate.getId());
+        }
+
+        public void thenTheExpectedAggregateShouldBeReturned() {
+            assertThat(actualAggregate).isEqualTo(aggregate);
+        }
+
+        public void andVerifyNoInteractionWithRestoreAggregateFromSnapshotAndEvents() {
+            then(restoreAggregateFromSnapshot).shouldHaveNoInteractions();
+        }
+
+        public void andVerifyNoInteractionWithRestoreAggregateFromEvents() {
+            then(restoreAggregateFromEvents).shouldHaveNoInteractions();
+        }
+
+        public void andVerifyNoInteractionWithDependenciesOtherThanEventStore() {
+            then(snapshotRepository).shouldHaveNoInteractions();
+            then(restoreAggregateFromEvents).shouldHaveNoInteractions();
+            then(restoreAggregateFromSnapshot).shouldHaveNoInteractions();
+
+        }
+
+        public void thenVerifyAllNecessaryInteractionsWithDependencies() {
+            interactions.forEach(Runnable::run);
+        }
+
+        public void andVerifyNoMoreInteractionsWithTheseDependencies() {
+            interactionObjects.forEach(interactionObject -> then(interactionObject).shouldHaveNoMoreInteractions());
+        }
+
+        private void addInteraction(Object interactionObject, Runnable interaction) {
+            interactionObjects.add(interactionObject);
+            interactions.add(interaction);
+        }
+
     }
 }
